b1e25873aee55f23a9e6e4d644a0e316a62cf233
Will Murphy <will.murphy@anchore.com>
2024-01-26 08:46:44 -0500

    expose WrapForTesting
    Signed-off-by: Will Murphy <will.murphy@anchore.com>

--- testutils/application.go
+++ testutils/application.go
@@ -2,41 +2,68 @@ package testutils
 
 import (
 	"github.com/anchore/clio"
+	"github.com/google/go-cmp/cmp"
 	"github.com/spf13/cobra"
+	"github.com/stretchr/testify/assert"
+	"sync"
 	"testing"
 )
 
-func NewForTesting(t *testing.T, cfg *clio.SetupConfig, assertion assertionFunc) clio.Application {
+// TODO: is this needed given WrapForTesting? We need to think about
+// how clio is handling global state.
+// in particular, the testing pattern can cause initializers to be called more than once.
+func NewForTesting(t *testing.T, cfg *clio.SetupConfig, assertions ...AssertionFunc) clio.Application {
 	a := clio.New(*cfg)
 
-	//type Tester interface {
-	//	WithTesting(assertion func(cfgs ...any)) clio.Application
-	//}
-	//
-	//var asserter func(...any) = func(cfgs ...any) {
-	//	assertion(t, cfgs)
-	//}
-	//
-	//a = a.(Tester).WithTesting(asserter)
+	var asserter assertionClosure = func(cmd *cobra.Command, args []string, cfgs ...any) {
+		for _, assertion := range assertions {
+			assertion(t, cmd, args, cfgs...)
+		}
+	}
 
 	return &testApplication{
 		a,
-		assertion,
-		t,
+		asserter,
+		sync.Once{},
 	}
 }
 
-type assertionFunc func(t *testing.T, cfgs ...any)
+func WrapForTesting(t *testing.T, a clio.Application, assertions ...AssertionFunc) clio.Application {
+	var asserter assertionClosure = func(cmd *cobra.Command, args []string, cfgs ...any) {
+		for _, assertion := range assertions {
+			assertion(t, cmd, args, cfgs...)
+		}
+	}
+
+	return &testApplication{
+		a,
+		asserter,
+		sync.Once{},
+	}
+}
+
+type AssertionFunc func(t *testing.T, cmd *cobra.Command, args []string, cfgs ...any)
+
+func OptionsEquals(opts any) AssertionFunc {
+	return func(t *testing.T, cmd *cobra.Command, args []string, cfgs ...any) {
+		assert.Equal(t, len(cfgs), 1)
+		if d := cmp.Diff(opts, cfgs[0]); d != "" {
+			t.Errorf("mismatched options (+got -want):\n%s", d)
+		}
+	}
+}
+
+type assertionClosure func(cmd *cobra.Command, args []string, cfgs ...any)
 
 type testApplication struct {
 	clio.Application
-	assertion assertionFunc
-	t         *testing.T
+	assertion assertionClosure
+	initOnce  sync.Once
 }
 
 func (a *testApplication) SetupCommand(cmd *cobra.Command, cfgs ...any) *cobra.Command {
 	cmd.RunE = func(cmd *cobra.Command, args []string) error {
-		a.assertion(a.t, cfgs...)
+		a.assertion(cmd, args, cfgs...)
 		return nil
 	}
 	return a.Application.SetupCommand(cmd, cfgs...)
@@ -44,8 +71,15 @@ func (a *testApplication) SetupCommand(cmd *cobra.Command, cfgs ...any) *cobra.C
 
 func (a *testApplication) SetupRootCommand(cmd *cobra.Command, cfgs ...any) *cobra.Command {
 	cmd.RunE = func(cmd *cobra.Command, args []string) error {
-		a.assertion(a.t, cfgs...)
+		a.assertion(cmd, args, cfgs...)
 		return nil
 	}
 	return a.Application.SetupRootCommand(cmd, cfgs...)
 }
+
+/*
+// TODO: WISHLIST:
+1. Pass in an io.Reader that's wired up to the config file, _or_ (almost as good) pass in a test fixture path
+2. Set env vars by passing map[string]string
+3. Helper to pass in a fully populated options struct and assert that it's correct
+*/
