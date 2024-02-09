cb94e40a4f659cf53878aaec0427e1cb08f61013
Keith Zantow <kzantow@gmail.com>
2024-02-09 15:47:44 -0500

    fix: output version to stdout instead of command out (#43)
    Signed-off-by: Keith Zantow <kzantow@gmail.com>

--- version_test.go
+++ version_test.go
@@ -1,10 +1,38 @@
 package clio
 
 import (
+	"bytes"
+	"io"
+	"os"
+	"sync"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
+
+func Test_versionOutputToStdout(t *testing.T) {
+	c := VersionCommand(Identification{
+		Name:    "test",
+		Version: "version",
+	})
+
+	stdout := &bytes.Buffer{}
+	restoreStdout := capture(&os.Stdout, stdout, 1024)
+	defer restoreStdout()
+
+	stderr := &bytes.Buffer{}
+	restoreStderr := capture(&os.Stderr, stderr, 1024)
+	defer restoreStderr()
+
+	_ = c.RunE(c, nil)
+
+	// close and flush the buffers, wait until complete
+	restoreStdout()
+	restoreStderr()
+
+	require.NotEmpty(t, stdout.String())
+	require.Empty(t, stderr.String())
+}
 
 func Test_versionInfoText(t *testing.T) {
 	expected := `Application:               the-name
@@ -66,4 +94,38 @@ func Test_versionInfoJSON(t *testing.T) {
 	})
 	require.NoError(t, err)
 	require.JSONEq(t, expected, got)
+}
+
+func capture(target **os.File, writer io.Writer, bufSize int) (close func()) {
+	original := *target
+
+	r, w, _ := os.Pipe()
+
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+
+	go func() {
+		defer wg.Done()
+		buf := make([]byte, bufSize)
+		for {
+			n, err := r.Read(buf)
+			if n > 0 {
+				_, _ = writer.Write(buf[0:n])
+			}
+			if err != nil {
+				break
+			}
+		}
+	}()
+
+	*target = w
+
+	return func() {
+		if original != nil {
+			_ = w.Close()
+			wg.Wait()
+			*target = original
+			original = nil
+		}
+	}
 }
